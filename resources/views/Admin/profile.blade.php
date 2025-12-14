42f66ed8bbdc54c0f74e616d7bdab1f0951c4772
ramadhanriefalldo0-idn <ramadhanriefalldo0@gmail.com>
2025-12-14 20:22:32 +0700

    nambahin foto profile,ubah warna chart card di dashboard admin

--- resources/views/Admin/profile.blade.php
+++ resources/views/Admin/profile.blade.php
@@ -3,27 +3,41 @@
 @section('title', 'Profil Admin')
 
 @section('content')
-    <div class="container px-4">
-        <div class="card">
-            <div class="card-body">
-                <h3 class="card-title">Profil Admin</h3>
+    <div class="container px-4 admin-profile">
+        <div class="card profile-card">
+            <div class="avatar">
+                @if(!empty($admin->avatar))
+                    <img src="{{ asset('storage/' . $admin->avatar) }}" alt="avatar">
+                @else
+                    <img src="{{ asset('images/profile .jpg') }}" alt="avatar">
+                @endif
+            </div>
+
+            <div class="profile-info">
+                <h3>{{ $admin->nama ?? 'Admin' }}</h3>
+                <div class="meta">Administrator</div>
+
+                <div class="profile-divider"></div>
 
                 <dl class="row">
-                    <dt class="col-sm-3">Nama</dt>
-                    <dd class="col-sm-9">{{ $admin->nama ?? '-' }}</dd>
-
                     <dt class="col-sm-3">Email</dt>
                     <dd class="col-sm-9">{{ $admin->email ?? '-' }}</dd>
 
-                    <dt class="col-sm-3">Role</dt>
-                    <dd class="col-sm-9">{{ $admin->role ?? 'admin' }}</dd>
-
                     <dt class="col-sm-3">No. HP</dt>
                     <dd class="col-sm-9">{{ $admin->no_hp ?? '-' }}</dd>
+
+                    <dt class="col-sm-3">Role</dt>
+                    <dd class="col-sm-9">{{ $admin->role ?? 'admin' }}</dd>
                 </dl>
 
-                <a href="{{ route('admin.dashboard') }}" class="btn btn-secondary">Kembali</a>
+                <div class="actions">
+                    <a href="{{ route('admin.dashboard') }}" class="btn btn-secondary">Kembali</a>
+                </div>
             </div>
         </div>
     </div>
 @endsection
+
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
+@endpush
